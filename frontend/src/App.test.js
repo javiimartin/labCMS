f591e905004ac422d1c32c7c8e1f2cf096fe56bf
Javier <jmartinalcalde@correo.ugr.es>
2025-01-07 10:54:26 +0100

    modifico test del frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const titleElement = screen.getByText(/Applied Business Research Department/i);
   expect(linkElement).toBeInTheDocument();
 });
